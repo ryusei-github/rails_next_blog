49db6b8df9aef7d2de3333abf6a97033f2fbe2b7
konor <ryuseialchemy@gmail.com>
2023-12-07 03:33:11 +0900

    add:api

---
+++ blog_api/app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::PostsController < ApplicationController
+  #コントローラーのの記述
+  def index
+    @posts = Post.all
+    redner json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+      else
+        render json @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post
+      else
+        render json @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
